d960fc64e86b25c0139d36778b8712af69261477
umama1991 <141228155+umama1991@users.noreply.github.com>
2023-10-04 03:32:16 -0700

    Update ordermanagement.py

--- ordermanagement.py
+++ ordermanagement.py
@@ -6,7 +6,7 @@ app.config["MYSQL_HOST"]="localhost"
 
 app.config["MYSQL_USER"]="root"
 
-app.config["MYSQL_PASSWORD"]="iloveyou@123@"
+app.config["MYSQL_PASSWORD"]=""
 
 app.config["MYSQL_DB"]="assignment"
 
